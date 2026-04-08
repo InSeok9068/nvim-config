23a10ee8d466937c4fb06fbda73a0ff05b906ba2
Lee In Seok <dlstjr9068@gmail.com>
2026-04-08 14:11:23 +0900

    lazyvim작ㅇ버

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+```shell
+scoop bucket add lewohy-bucket https://github.com/lewohy/lewohy-bucket
+scoop install kren-select
+```
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,25 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local im = "kren-select"
+local last = "en"
+
+-- Insert 나갈 때
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    -- 현재 상태 저장 (en / kr)
+    last = vim.fn.system(im):gsub("%s+", "")
+
+    -- 영어로 강제
+    vim.fn.system(im .. " en")
+  end,
+})
+
+-- Insert 들어갈 때
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    -- 이전 상태 복구
+    vim.fn.system(im .. " " .. last)
+  end,
+})
