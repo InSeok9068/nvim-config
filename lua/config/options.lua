357e35f7d69d0a74e09c6dc8d09c531ee284d806
Lee In Seok <dlstjr9068@gmail.com>
2026-04-08 15:48:45 +0900

    옵션 변경

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
+vim.o.shellcmdflag = "-c"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
