5119839cde0fd282001e66f0c75417cb63a6f62a
Lee In Seok <dlstjr9068@gmail.com>
2026-04-09 16:20:34 +0900

    ejs 하이라이팅

---
+++ lua/plugins/ejs.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    init = function()
+      vim.filetype.add({
+        extension = {
+          ejs = "ejs",
+        },
+      })
+
+      vim.treesitter.language.register("embedded_template", "ejs")
+    end,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      if not vim.tbl_contains(opts.ensure_installed, "embedded_template") then
+        table.insert(opts.ensure_installed, "embedded_template")
+      end
+    end,
+  },
+}
